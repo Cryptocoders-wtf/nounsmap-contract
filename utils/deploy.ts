7e11d6a38ea256b77aa80864757af31a66a5ac8e
TAK_Ryuji <kozayupapa@gmail.com>
2022-07-06 05:55:50 +0900

    apply for each network

--- utils/deploy.ts
+++ utils/deploy.ts
@@ -1,7 +1,5 @@
 import { ethers, network } from "hardhat";
 
-const descriptor = "0x0cfdb3ba1694c2bb2cfacb0339ad7b1ae5932b63";
-const seeder = "0xcc8a0fb5ab3c7132c1b2a0109142fb112c4ce515";    
 export const developer = "0x3e7311e0Fc89fA633433076be268ae007A1b827a"; 
 export const committee = "0x3e7311e0Fc89fA633433076be268ae007A1b827a"; 
 
@@ -19,15 +17,49 @@ const priceSeed = {
   expirationTime: 90 * 60, // 90 minutes;
 };
 export const deploy:any = async (setWhitelist = true) => {
- 
-  const nounsTokenFactory = await ethers.getContractFactory("NounsToken");
-  const authorityToken = await nounsTokenFactory.deploy(descriptor,seeder,developer,committee,priceSeed,proxy);
-  await authorityToken.deployed();
-
+  let i =0
+  console.log(i++);
   const contentsTokenFactory = await ethers.getContractFactory("ContentsToken");
-  const contentsToken = await contentsTokenFactory.deploy(descriptor,seeder,developer,committee,priceSeed,proxy);
+  const contentsToken = await contentsTokenFactory.deploy(developer,committee,priceSeed,proxy);
   await contentsToken.deployed();
-  await contentsToken.addAuthority(authorityToken.address);
 
+  console.log(i++);
+  const nounsTokenFactory = await ethers.getContractFactory("NounsToken");
+  console.log(i++);
+
+  let authorityToken;
+  if((network.name == "hardhat")){
+    const descriptor = "0x0cfdb3ba1694c2bb2cfacb0339ad7b1ae5932b63";
+    const seeder = "0xcc8a0fb5ab3c7132c1b2a0109142fb112c4ce515";    
+    const proxy = "0xa5409ec958c83c3f309868babaca7c86dcb077c1"; // openSea proxy
+    authorityToken = await nounsTokenFactory.deploy(descriptor,seeder,developer,committee,priceSeed,proxy,{gasLimit: 9000000});
+    console.log(i++);
+    await authorityToken.deployed();  
+  await authorityToken.deployed();
+    await authorityToken.deployed();  
+    console.log(i++);
+    await contentsToken.addAuthority(authorityToken.address);
+  } 
+  console.log(i++);
+  if((network.name == "rinkeby")){
+    const descriptor = "0x292c84894c1B86140A784eec99711d6007005f21";
+    const seeder = "0x5bcc91c44bffa15c9b804a5fd30174e8da296a4b";
+    const proxy = "0xf57b2c51ded3a29e6891aba85459d600256cf317";    
+    authorityToken = await nounsTokenFactory.deploy(descriptor,seeder,developer,committee,priceSeed,proxy,{gasLimit: 9000000});
+    console.log(i++);
+    await authorityToken.deployed();  
+    console.log(i++);
+    await contentsToken.addAuthority(authorityToken.address);
+    const nounsToken = "0x1602155eB091F863e7e776a83e1c330c828ede19"; //NounsLove    
+    await contentsToken.addAuthority(nounsToken);
+  }
+  console.log(i++);
+
+  if((network.name == "mainnet")){
+    const nounsToken = "0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03"; //Nouns本家    
+    await contentsToken.addAuthority(nounsToken);
+  }
+  console.log(i++);
+  contentsToken.setWeb2("https://dev.nounsmap.com/p/");
   return { contentsToken, authorityToken };
 };
